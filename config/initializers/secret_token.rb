340e7d3cbf3cef7c00663110b2b43446e32e4010
Matt Solano <Mattio2187@aol.com>
2013-01-03 11:34:29 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Up4grabs::Application.config.secret_token = '81e6b9b77e7acb21ca3363e08e846dcfb404874e477741a1c31f979e82105a352e6dc4644441ef3343206354bb8e314eb491ea9be00a8eb011474c0564dc1836'
